19e991dc42212a86e766dca705d01c296c93035c
Ravi Rai <ravirairgstartup@gmail.com>
2026-03-06 18:25:26 +0530

    build error

--- src/pages/TilesCategory.jsx
+++ src/pages/TilesCategory.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 import bathroomImg from "../assets/images/bathroom.webp";
 import kitchenImg from "../assets/images/kitchen.webp";
